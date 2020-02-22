f5b376d9d8b1067fef35e8ce4064b75bbda46b9f
ayushjain1144 <ayushjain1144@gmail.com>
2020-02-22 19:33:24 +0530

    1st task almost complete

--- part1.py
+++ part1.py
@@ -1,7 +1,12 @@
 import random
-import numpy
+import numpy as np
 from dataclasses import dataclass
 import time
+
+# 7 dollars per hour or 0.002 dollars / sec
+OPPORTUNITY_COST  =  0.002
+EPSILON = 0.1
+MIN_BOUND = 0.5
 
 @dataclass
 class Gamble:
@@ -54,41 +59,79 @@ def get_reward(g1, g2, heurestic, roulette_wheel):
 
 def get_time_cost(heurestic):
     
-    
+    # 4 Reads + 2 Additions + 1 comparisions + 1 Result
     if heurestic == 'ewh':
-        return 3
+        return 8 * OPPORTUNITY_COST
+
+    # 2 Reads + 1 comparision + 1 Result
     else:
-        return 1
+        return 4 * OPPORTUNITY_COST
 
-def get_relative_reward(g1, g2, heurestic, roulette_wheel):
+def extract_features(g1):
 
-    actual_reward = get_reward(g1, g2, heurestic, roulette_wheel)
+    highest_outcome_probability = g1.p
+    differnce = max(g1.x1, g1.y1) - min (g1.x1, g1.y1)
+    return (highest_outcome_probability, differnce)
 
-    if roulette_wheel < g1.p:
-        max_reward_strategy = max(g1.x, g2.x)
-    else:
-        max_reward_strategy = max(g1.y, g2.y)
+
+def get_relative_reward(g1, g2, heurestic):
+
     
-    predicted_relative_reward =  (actual_reward / max_reward_strategy)
+    # outcome can be either o1 (with prob p) and o2 (with prob 1-p)
+
+
+    #if outcome is o1
+
+    actual_reward_o1 = get_reward(g1, g2, heurestic, 0)
+    max_reward_o1 = max(g1.x, g2.x)
+    predicted_relative_reward_o1 =  (actual_reward_o1 / max_reward_o1)
+
+    # if outcome is o2
+    actual_reward_o2 = get_reward(g1, g2, heurestic, 1)
+    max_reward_o2 = max(g1.y, g2.y)
+    predicted_relative_reward_o2 =  (actual_reward_o2 / max_reward_o2)
+
+    # expected reward = o1*p + o2*(1-p)
+
+    expected_reward = predicted_relative_reward_o1 * p + predicted_relative_reward_o2 * (1 - p)
 
     min_possible_reward = min(g1.x, g1.y, g2.x, g2.y)
     max_possible_strategy = max(g1.x, g1.y, g2.x, g2.y)
-    predicted_absolute_reward = min(min_possible_reward + (max_possible_strategy - min_possible_reward) * predicted_relative_reward, max_possible_strategy)
+    predicted_absolute_reward = min(min_possible_reward + (max_possible_strategy - min_possible_reward) * expected_reward, max_possible_strategy)
 
-    return predicted_absolute_reward 
+    if heurestic == 'ewh':
+        return predicted_absolute_reward / 8
+    else:
+        return predicted_absolute_reward / 4
+
 
 
 # VOC = (utility of strategy s) - gamma * cost
-def get_voc(g1, g2, heurestic, roulette_wheel, reward, t):
-    utility = get_reward(g1, g2, heurestic, roulette_wheel)
-    gamma = get_relative_reward(g1, g2, heurestic, roulette_wheel)
+def get_voc(g1, g2, heurestic):
+    utility = get_relative_reward(g1, g2, heurestic)
     cost = get_time_cost(heurestic)
-    voc = utility - (gamma * cost)
+    voc = utility - cost
     #print(gamma, voc, heurestic)
     return voc
     
 
+def get_strategy_epsilon_greedy(voc_ewh, voc_lh):
 
+    # exploit
+    if random.random() < 1 - EPSILON:
+        if np.argmax((voc_ewh, voc_lh)) == 0:
+            return "ewh"
+        else:
+            return "lh"
+    
+    #explore
+    else:
+        if random.random() < 0.5:
+            return "ewh"
+        else:
+            return "lh"
+
+    
 
 
 #################### main function #################
@@ -102,7 +145,9 @@ count_lh = 0
 voc_lh_total = 0
 voc_ewh_total = 0
 while t < 1000:
-    p = random.uniform(0.9, 1.0)
+
+    print(f"\nPresent time:  {t}\n")
+    p = random.uniform(MIN_BOUND , 1.0)
     x1 = random.uniform(-10, 10)
     y1 = random.uniform(-10, 10)
     x2 = random.uniform(-10, 10)
@@ -111,67 +156,30 @@ while t < 1000:
     g1 = Gamble(p, x1, y1)
     g2 = Gamble(p, x2, y2)
 
+    # for selecting the actual outcome, similar to what is done in genetic algorithms
     roulette_wheel = random.random()
 
-    voc_ewh = get_voc(g1, g2, 'ewh', roulette_wheel, reward, t)
-    voc_lh = get_voc(g1, g2, 'lh', roulette_wheel, reward, t)
-    print(voc_ewh, voc_lh)
-    voc_lh_total = voc_lh + voc_lh_total
-    voc_ewh_total = voc_ewh + voc_ewh_total
+    voc_ewh = get_voc(g1, g2, 'ewh')
+    voc_lh = get_voc(g1, g2, 'lh')
 
-    if voc_ewh > voc_lh:
-        t = t + 3
-        reward = reward + get_reward(g1, g2, 'ewh', roulette_wheel)
+    heurestic = get_strategy_epsilon_greedy(voc_ewh, voc_lh)
+    print(f"usinh heurestic: {heurestic}")
+    reward = reward + get_reward(g1, g2, heurestic, roulette_wheel)
+    print(f"got reward: {reward}")
+    #print(voc_ewh, voc_lh, g1, g2)
+
+    
+    if heurestic == "ewh":
+        t = t + 8
         count_ewh = count_ewh + 1
-        #print('ewh')
     else:
-        t = t + 1
-        reward = reward + get_reward(g1, g2, 'lh', roulette_wheel)
+        t = t + 4
         count_lh = count_lh + 1
-        #print('lh')
-    #print(t, g1.x, g1.y, g2.x, g2.y, reward)
-    
+
+print("\nFinal statistics\n\n")
+
+print(f"\n\ntotal reward: {reward}")
+print(f"number of times equal weight heurestic is used: {count_ewh}")
+print(f"number of lexigographic heurestic is used: {count_lh}")
 
 
-def run():
-    reward = 0
-
-    t = 0
-    count_ewh = 0
-    count_lh = 0
-
-    voc_lh_total = 0
-    voc_ewh_total = 0
-    while t < 1000:
-        p = random.uniform(0.9, 1.0)
-        x1 = random.uniform(-10, 10)
-        y1 = random.uniform(-10, 10)
-        x2 = random.uniform(-10, 10)
-        y2 = random.uniform(-10, 10)
-        
-        g1 = Gamble(p, x1, y1)
-        g2 = Gamble(p, x2, y2)
-
-        roulette_wheel = random.random()
-
-        voc_ewh = get_voc(g1, g2, 'ewh', roulette_wheel, reward, t)
-        voc_lh = get_voc(g1, g2, 'lh', roulette_wheel, reward, t)
-        print(voc_ewh, voc_lh)
-        voc_lh_total = voc_lh + voc_lh_total
-        voc_ewh_total = voc_ewh + voc_ewh_total
-
-        if voc_ewh > voc_lh:
-            t = t + 3
-            reward = reward + get_reward(g1, g2, 'ewh', roulette_wheel)
-            count_ewh = count_ewh + 1
-            #print('ewh')
-        else:
-            t = t + 1
-            reward = reward + get_reward(g1, g2, 'lh', roulette_wheel)
-            count_lh = count_lh + 1
-
-    print(voc_ewh_total, voc_lh_total)
-#print(reward)
-#print(count_ewh, count_lh)
-
-    
